3147fa64bda6ecb1947547d5365706b93874e0e2
Marina Herrera Sarrias <mahe9165@roos.math.su.se>
2026-06-01 10:42:56 +0200

    Update target directory path in fetch_GRCh38.115_unmasked.sh

--- scripts/fetch_GRCh38.115_unmasked.sh
+++ scripts/fetch_GRCh38.115_unmasked.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-TARGET_DIRECTORY='/home/mahe9165/data/exonize/ensembl/v115/data'
+TARGET_DIRECTORY='/home/mahe9165/exonize/data/ensembl/v115/'
 
 mkdir -p "$TARGET_DIRECTORY"
 
 # Homo_sapiens
 ## annotations
-curl -L -o "$TARGET_DIRECTORY/Homo_sapiens.GRCh38.115.chr.gff3.gz" \
-  "https://ftp.ensembl.org/pub/release-115/gff3/homo_sapiens/Homo_sapiens.GRCh38.115.chr.gff3.gz"
+curl -L -o "$TARGET_DIRECTORY/Homo_sapiens.GRCh38.115.gff3.gz" \
+  "https://ftp.ensembl.org/pub/release-115/gff3/homo_sapiens/Homo_sapiens.GRCh38.115.gff3.gz"
 
 ## genome
 curl -L -o "$TARGET_DIRECTORY/Homo_sapiens.GRCh38.dna.toplevel.fa.gz" \
