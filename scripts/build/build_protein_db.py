1730f459d1a9f396659469742b681809e20b2f6a
Marina Herrera Sarrias <mahe9165@roos.math.su.se>
2026-06-08 16:01:45 +0200

    scripts for producing protein db

---
+++ scripts/build/build_protein_db.py
@@ -0,0 +1,63 @@
+import argparse
+from prot_utils import *
+from prot_sql_utils import *
+
+
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--gene-hierarchy-dictionary-path',
+        type=str,
+        help='Path to gene hierarchy dictionary.',
+    )
+    parser.add_argument(
+        '--genome-path',
+        type=str,
+        help='Path to genome.',
+    )
+    parser.add_argument(
+        '--protein-db-path',
+        type=str,
+        help='Path to protein database.',
+    )
+    parser.add_argument(
+        '--timeout-db',
+        type=int,
+        help='Timeout for database.',
+    )
+    return parser.parse_args()
+
+
+def read_pkl_file(
+        file_path: str,
+) -> dict:
+    with open(file_path, 'rb') as handle:
+        read_file = pickle.load(handle)
+    return read_file
+
+
+def main():
+    args = args_parser()
+    gene_hierarchy_dictionary_path = Path(args.gene_hierarchy_dictionary_path)
+    genome_path = Path(args.genome_path)
+    protein_db_path = Path(args.protein_db_path)
+    timeout_db = args.timeout_db
+    gene_hierarchy_dictionary = read_pkl_file(
+        file_path=Path(gene_hierarchy_dictionary_path)
+    )
+    genome_dictionary = read_genome(
+        genome_file_path=genome_path
+    )
+    create_mrna_transcripts_cds_tables(
+        db_path=protein_db_path
+    )
+    populate_cds_and_mrna_tables(
+        gene_hierarchy_dictionary=gene_hierarchy_dictionary,
+        genome_dictionary=genome_dictionary,
+        protein_db_path=protein_db_path,
+        timeout_db=timeout_db
+    )
+
+
+if __name__ == '__main__':
+    main()
