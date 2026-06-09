561760011ac69a87ef61be110fb7081460c15181
Marina Herrera Sarrias <marina.sarrias@math.su.se>
2026-06-09 14:57:29 +0200

    fix formatting in build_human_prot_db.sh

--- scripts/run/build_human_prot_db.sh
+++ scripts/run/build_human_prot_db.sh
@@ -4,6 +4,6 @@ BASE=/home/mahe9165/exon-structure-evolution/data/ensembl/v115
 
 python3 build_protein_db.py \
     --gene-hierarchy-dictionary-path $BASE/GRCh38.115_gene_hierarchy.pkl \
-    --protein-db-path                $BASE/GRCh38.115_protein.db \
-    --genome-path                    $BASE/Homo_sapiens.GRCh38.dna.toplevel.fa.gz \
-    --timeout-db                     30
+    --protein-db-path $BASE/GRCh38.115_protein.db \
+    --genome-path $BASE/Homo_sapiens.GRCh38.dna.toplevel.fa.gz \
+    --timeout-db 30
